b46b3820513e62c98176a1517ce821dfade3a8ac
Sunil Behera <sunil.6318ie@gmail.com>
2023-03-28 10:44:50 +0530

    implemented use state and search functionilty in react food app

--- motabhai/src/Components/Body.js
+++ motabhai/src/Components/Body.js
@@ -1,15 +1,32 @@
 import Cards from './Cards';
 import { restaurantList } from '../../constants';
+import { useState } from 'react';
+
+filterRestaurant = (searchText, restaurants) => {
+    const restros = restaurants.filter(restau => restau.data.name.includes(searchText));
+    return restros;
+};
 
 const Body = () => {
+    const [searchText, setSearchText] = useState('');
+    const [restaurants, setRestaurants] = useState(restaurantList);
     return (
-        <div className="cards-container">
-            {
-                restaurantList.map(item => 
-                    <Cards restaurant={item} key={item.data.uuid}/>
-                )
-            }
-        </div>
+        <>
+            <div>
+                <input placeholder='Search' onChange={(e) => setSearchText(e.target.value)}/>
+                <button onClick={() => {
+                    const data = filterRestaurant(searchText, restaurantList);
+                    setRestaurants(data);
+                }}>Search</button>
+            </div>
+            <div className="cards-container">
+                {
+                    restaurants.map(item => 
+                        <Cards restaurant={item} key={item.data.uuid}/>
+                    )
+                }
+            </div>
+        </>
     )
 };
 
